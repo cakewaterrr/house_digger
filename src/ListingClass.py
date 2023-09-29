613a06f7e9a0e0bfbfd5da6114e1c45aa98772fe
Savannah Stephenson <stephensonsavannah@ymail.com>
2023-09-29 01:22:10 -0500

    writing out some parameter definitons

--- src/ListingClass.py
+++ src/ListingClass.py
@@ -11,3 +11,25 @@
 #----------------------------------
 # 
 #Imports
+from datetime import datetime
+
+#notes: i was doing this.... and then realized it was silly goofy to try and 
+#build the object without knowing what it was actual feasible to pull from sites
+class Listing():
+    """A Listing is an object used to store the data about a listing that has been scraped from a real estate website
+        :param estate_type: str - The kind of real estate that the property is, house, apartment, duplex ect.
+        :param money_type: str - If the listing is up for rent or to buy. 
+        :param market_price: str -
+        :param rent_price: str - 
+        :param square_feet: int - The size of the property in terms of square feet. 
+        :parm price_per_square_foot: int - The price per square foot. 
+        :param num_bedrooms: int - The number of bedrooms in the property. 
+        :param num_bathrooms: int - The number of bathrooms in the property. 
+        :param address: str - The address of the listing. 
+        :param hoa: bool - If there is a home owner's association associated with he property.   
+        :param listing-age: int - How long the property has been on the market. 
+        :
+
+    """
+    def __init__() -> None:
+       pass
