a4407dc3c3cbe673aedcc43a14063f36911e7706
RankedGabe <gabrielcavazos9@gmail.com>
2023-10-23 16:16:37 -0500

    pulling from dev

--- src/format_results.py
+++ src/format_results.py
@@ -1,4 +1,4 @@
-#format_results.py
+#format_resutls.py
 #----------------------------------
 # Created By: Savannah Stephenson
 # Created Date: 9/24/2023
@@ -6,7 +6,7 @@
 # Edited By: 
 # Edit Date: 
 #----------------------------------
-"""This file prints the gathered list of ListingClass objects to screen in a nice way. 
+"""This file receives a list of ListingClass objects and prints the data to the screen. 
  """ 
 #----------------------------------
 # 
