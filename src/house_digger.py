179a1ba91d662aad35b0fbc1d39ac2d1cfeb7f3c
aliciamares <101082445+aliciamares@users.noreply.github.com>
2023-10-26 13:32:40 -0500

    Update house_digger.py

--- src/house_digger.py
+++ src/house_digger.py
@@ -3,11 +3,21 @@
 # Created By: Savannah Stephenson
 # Created Date: 9/24/2023
 #----------------------------------
-# Edited By: 
-# Edit Date: 
+# Edited By: Savannah Stephenson
+# Edit Date: 10/16/2023
+# Note: Maybe add a loading screen while the preferences are being added
 #----------------------------------
-"""This file acts as the runner file for house_digger.  
+"""This file acts as the runner file for House Digger.
  """ 
 #----------------------------------
 # 
 #Imports
+from preference import *
+from gather_data import *
+
+#Gathering User's Preferences
+sorting_criteria = gather_preferences()
+
+#Searching according to criteria on redfin.com and printing
+print("redfin.com Listings:")
+print(gather_data_from_redfin(sorting_criteria))
