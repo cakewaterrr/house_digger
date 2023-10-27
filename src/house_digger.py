a97102435f2e2831ff235a28d556e1b287bef13c
aliciamares <101082445+aliciamares@users.noreply.github.com>
2023-10-26 22:37:41 -0500

    Update house_digger.py

--- src/house_digger.py
+++ src/house_digger.py
@@ -14,6 +14,8 @@
 #Imports
 from preference import *
 from gather_data import *
+#from gather_data2 import *
+#from gather_data3 import *
 
 #Gathering User's Preferences
 sorting_criteria = gather_preferences()
@@ -21,3 +23,7 @@ sorting_criteria = gather_preferences()
 #Searching according to criteria on redfin.com and printing
 print("redfin.com Listings:")
 print(gather_data_from_redfin(sorting_criteria))
+#print("realtor.com Listings:")
+#print(gather_data_from_realtor(sorting_criteria))
+#print("trulia.com Listings:")
+#print(gather_data_from_trulia(sorting_criteria))
